4db97921daf6e13310ea9c743933f378cd6a1dab
Armfath <info.armanagonou@gmail.com>
2023-04-24 14:47:20 +0100

    gather infoo from API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Python package requests using to deal with APIs
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    value = sys.argv[1]
+    param = {'userId': value}
+    todo_r = requests.get(url+f'{value}/todos')
+    info_r = requests.get(url+f'{value}')
+    done = []
+    for todo in todo_r.json():
+        if todo.get('completed') is True:
+            done.append(todo)
+    print('Employee {} is done with tasks({}/{}):'
+          .format(info_r.json().get('name'), len(done), len(todo_r.json())))
+    for task in done:
+        print('\t{}'.format(task.get('title')))
