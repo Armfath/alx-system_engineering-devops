d0e550ba94d0d9cbb882e23aeab478da5fe364ba
Armfath <info.armanagonou@gmail.com>
2023-04-25 03:16:16 +0100

    new test export json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 14 16:22:14 2020
-
-@author: Robinson Montes
-"""
-from json import dump
-from requests import get
-from sys import argv
+'''
+Python script that, using this REST API, for a given employee ID
+'''
+import json
+import requests
+import sys
 
 
-if __name__ == '__main__':
-    user_id = argv[1]
-    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-    response = get(url)
-    username = response.json().get('username')
-
-    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
-    response = get(url)
-    tasks = response.json()
-    dictionary = {user_id: []}
+if __name__ == "__main__":
+    userID = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(userID)
+    tasks = requests.get('{}/todos'.format(url)).json()
+    info = requests.get(url).json()
+    list = []
     for task in tasks:
-        dictionary[user_id].append({
-                                    "task": task.get('title'),
-                                    "completed": task.get('completed'),
-                                    "username": username
-                                    })
-    with open('{}.json'.format(user_id), 'w') as file:
-        dump(dictionary, file)
+        data = {}
+        data["task"] = task.get('title')
+        data["completed"] = task.get('completed')
+        data["username"] = info.get('username')
+        list.append(data)
+    datas = {}
+    datas[userID] = list
+    filename = '{}.json'.format(userID)
+    with open(filename, mode='w') as jsonfile:
+        json.dump(datas, jsonfile)
