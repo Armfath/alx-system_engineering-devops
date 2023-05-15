03cfc1d68e9c2c199d6c625e720b3e6852aa3fff
Armfath <info.armanagonou@gmail.com>
2023-05-15 15:50:15 +0100

    solve wordpress error on apache2 server

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,5 @@
 exec { 'fix-wordpress':
   command => 'cp /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
   onlyif  => 'test -f /var/www/html/wp-includes/class-wp-locale.php',
-  path    => ['/bin', '/usr/bin'], needed
+  path    => ['/bin', '/usr/bin'],
 }
