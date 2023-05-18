54053757917a1173daf1f340703e01bec580d9f0
Armfath <info.armanagonou@gmail.com>
2023-05-18 08:48:10 +0100

    add command to restart nginx after modifucation

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,3 +4,8 @@ exec { 'fix-for-nginx-ulimit':
   command => 'sed -i "s/ULIMIT=\"-n.*\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
   path    => ['/bin', '/usr/bin', '/usr/local/bin'],
 }
+
+exec { 'restart':
+  path    => '/usr/bin/',
+  command => 'service nginx restart',
+}
