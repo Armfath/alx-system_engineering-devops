8a19c7d2e1c0513a2d094eef4eaa7cca730bcd3f
Armfath <info.armanagonou@gmail.com>
2023-05-19 11:38:09 +0100

    extra line at end deleted

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,4 +4,3 @@ exec { 'fix-for-nginx-ulimit':
   command => 'sed -i "s/ULIMIT=\"-n.*\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx && sed -i "s/worker_connections.*/worker_connections 1024;/" /etc/nginx/nginx.conf && service nginx restart',
   path    => ['/bin', '/usr/bin', '/usr/local/bin'],
 }
-
