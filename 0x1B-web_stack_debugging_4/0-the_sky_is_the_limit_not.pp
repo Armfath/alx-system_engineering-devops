e538a72a0a0e27cc20568e622c11f55bed9c864a
Armfath <info.armanagonou@gmail.com>
2023-05-19 11:37:35 +0100

    gather commands cause sys inverse order of executi

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,7 @@
 # Modify the maximun number of file allowed to be open
 
 exec { 'fix-for-nginx-ulimit':
-  command => 'sed -i "s/ULIMIT=\"-n.*\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  command => 'sed -i "s/ULIMIT=\"-n.*\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx && sed -i "s/worker_connections.*/worker_connections 1024;/" /etc/nginx/nginx.conf && service nginx restart',
   path    => ['/bin', '/usr/bin', '/usr/local/bin'],
 }
 
-exec { 'restart':
-  path    => '/usr/bin/',
-  command => 'service nginx restart',
-}
