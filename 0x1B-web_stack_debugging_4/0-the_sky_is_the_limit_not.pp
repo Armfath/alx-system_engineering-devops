ef5c647275a091e6265147dcbbb4a0b94379c673
Armfath <info.armanagonou@gmail.com>
2023-05-19 12:17:13 +0100

    Comment missing

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,4 @@
+# Modify the maximun number of file allowed to be open
 exec { 'increase-ulimit':
   command => 'sed -i "s/ULIMIT=\"-n.*\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
   path    => ['/bin', '/usr/bin', '/usr/local/bin'],
