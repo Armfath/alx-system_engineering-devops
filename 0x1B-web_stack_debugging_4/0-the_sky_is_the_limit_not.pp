8c1f94447b955e66226a9a16756dcdc1bd801018
Armfath <info.armanagonou@gmail.com>
2023-10-20 04:00:13 +0100

    fix: 0-codestyle

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,10 @@
-# Fix the nginx requests limit
+# Increase the request limit in nginx configuration
 exec { 'upgrade':
   path    => '/bin/',
   command => 'sed -i "s/15/4096" /etc/default/nginx',
 }
 
+# Restart the nginx service after making changes
 exec { 'restart':
   path    => '/usr/bin/',
   command => 'service nginx restart',
